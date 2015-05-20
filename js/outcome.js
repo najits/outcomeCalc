f6c74533d6aa2fe1644f4c283aa8f938283320e7
najits <najitsaria@gmail.com>
2015-05-20 12:44:05 -0400

    replaced net flow with separate savings and withdrawals

--- js/outcome.js
+++ js/outcome.js
@@ -12,15 +12,26 @@ function isValidNumericInput(n) {
 * args.portfolioReturnMean: mean portfolio return (% annual)
 * args.portfolioReturnStDev: std dev of portfolio returns (% annual)
 * args.initialCapital: starting capital amount
-* args.netMonthlyFlow: net of capital inflow and outflow per month
+* args.annualSavingsAmt: savings adding to intial capital (annual)
+* args.savingsDuration: remaining periods of additional savings (years)
+* args.monthlyWithdrawalAmt: withdrawals required from portfolio (monthly)
+* args.withdrawalStartOffset: period in which withdrawals being (years)
 **/
 var calcOutcomes = function(args) {
 	/* Process arguments */
 
 	// Normal random generated seeded with monthly portfolio return and volatility
-	if(!isNumeric(args.portfolioReturnMean)) { return; }
-	if(!isNumeric(args.portfolioReturnStDev) || (args.portfolioReturnStDev < 0)) { return; }
-	var rand = d3.random.normal(args.portfolioReturnMean / 100, args.portfolioReturnStDev / 100);
+	if(!isNumeric(args.portfolioReturnMean)) {
+		return;
+	} else {
+		var portfolioReturnMean = args.portfolioReturnMean / 100;
+	}
+	if(!isNumeric(args.portfolioReturnStDev) || (args.portfolioReturnStDev < 0)) {
+		return;
+	} else {
+		var portfolioReturnStDev = args.portfolioReturnStDev / 100;
+	}
+	var rand = d3.random.normal(portfolioReturnMean, portfolioReturnStDev);
 
 	// Simulation paths
 	var maxPaths = 10000;
@@ -33,15 +44,17 @@ var calcOutcomes = function(args) {
 	var numPeriods = isValidNumericInput(args.periods) ? Math.min(args.periods, maxPeriods) : defaultPeriods;
 
 	// Inital capital
-	var maxInitalCapital = 1e10;
 	var defaultInitialCapital = 1e06;
-	var initialCapital = isValidNumericInput(args.initialCapital) ? Math.min(args.initialCapital, maxInitalCapital) : defaultInitialCapital;
+	var initialCapital = isValidNumericInput(args.initialCapital) ? args.initialCapital : defaultInitialCapital;
 
-	// Monthly flow
-	var defaultMonthlyFlow = 0;
-	var monthlyFlow = (args.netMonthlyFlow && isNumeric(args.netMonthlyFlow)) ? args.netMonthlyFlow : defaultMonthlyFlow;
-	monthlyFlow *= 12;
+	// Savings
+	var annualSavingsAmt = (args.annualSavingsAmt && isNumeric(args.annualSavingsAmt)) ? args.annualSavingsAmt : 0;
+	var savingsDuration = isValidNumericInput(args.savingsDuration) ? args.savingsDuration : 0;
 
+	// Withdrawals
+	var _monthlyWithdrawalAmt = (args.monthlyWithdrawalAmt && isNumeric(args.monthlyWithdrawalAmt)) ? args.monthlyWithdrawalAmt : 0;
+	var annualWithdrawalAmt = _monthlyWithdrawalAmt * 12;
+	var withdrawalStartOffset = isValidNumericInput(args.withdrawalStartOffset) ? args.withdrawalStartOffset : 0;
 
 	/* Create matrices */
 
@@ -56,7 +69,11 @@ var calcOutcomes = function(args) {
 	// Fill nav matrix (periods + 1 x paths) with starting capital amount
 	var navMatrix = math.add(math.zeros(numPeriods + 1, numPaths), initialCapital);
 
-	// Initalize result arrays (per period)
+	// Flow vector to contain net of savings and withdrawals
+	var flowVector = [];
+	flowVector[0] = 0;
+
+	// Initalize arrays for 'per period' summary metrics
 	var alivePct = [];
 	alivePct[0] = numPaths;
 	var q25 = []
@@ -70,15 +87,19 @@ var calcOutcomes = function(args) {
 	/* Simulate NAVs */
 
 	// Cycle through per period, per path
-	var index, slice, sliceOrd, i, j;
+	var index, slice, i, j;
 	for(i = 0; i < numPeriods; i++) {
 		j = i + 1;
 		alivePct[j] = 0;
 
+		flowVector[j] = 0;
+		flowVector[j] += (j <= savingsDuration) ? annualSavingsAmt : 0;
+		flowVector[j] -= (j > withdrawalStartOffset) ? annualWithdrawalAmt : 0;
+
 		index = math.index(i, [0, numPaths]);
 		slice = math.add(
 					math.dotMultiply(navMatrix.subset(index), returnsMatrix.subset(index)),
-					monthlyFlow)
+					flowVector[j])
 						.map(function(value, index, matrix) {
 							if(value > 0) {
 								alivePct[j] += 1;
@@ -88,12 +109,12 @@ var calcOutcomes = function(args) {
 							}
 						});
 
-		sliceOrd = slice._data[0].sort(d3.ascending);
-		q25[j] = d3.quantile(sliceOrd, 0.25);
-		q50[j] = d3.quantile(sliceOrd, 0.50);
-		q75[j] = d3.quantile(sliceOrd, 0.75);
-
 		navMatrix.subset(math.index(j, [0, numPaths]), slice);
+
+		slice._data[0].sort(d3.ascending);
+		q25[j] = d3.quantile(slice._data[0], 0.25);
+		q50[j] = d3.quantile(slice._data[0], 0.50);
+		q75[j] = d3.quantile(slice._data[0], 0.75);
 	}
 
 	// Convert 'alive' to percentage of scenarios that are successful
@@ -110,6 +131,17 @@ var calcOutcomes = function(args) {
 	/* Collate results and return */
 
 	var results = {};
+	results.paramsUsed = {
+		numPaths: numPaths,
+		numPeriods: numPeriods,
+		initialCapital: initialCapital,
+		annualSavingsAmt: annualSavingsAmt,
+		savingsDuration: savingsDuration,
+		annualWithdrawalAmt: annualWithdrawalAmt,
+		withdrawalStartOffset: withdrawalStartOffset,
+		portfolioReturnMean: portfolioReturnMean,
+		portfolioReturnStDev: portfolioReturnStDev
+	};
 	results.alivePct = alivePct;
 	results.q25 = q25;
 	results.q50 = q50;
